798ba6a48cc516a6344b6c3a024d779734bbf02d
Mvfm <mvfm.filho@hotmail.com>
2025-10-28 23:09:17 +0000

    Pra poder rodar no BeeCrowd.

--- Cap 1 - Iniciante/bee1001.java
+++ Cap 1 - Iniciante/bee1001.java
@@ -1,15 +1,16 @@
 // Soma de valores 'a' & 'b' guardados em 'x', 'x' printado.
 
-import java.util.*;
+import java.io.*;
 
 public class bee1001{
-    public static void main(String[] args){
-        Scanner teclado = new Scanner(System.in);
-        int a = teclado.nextInt(); 
-        int b = teclado.nextInt();
-        teclado.close();
-
-        int x = a + b;
-        System.out.println(x);
+    public static void main(String[] args) throws IOException{
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader buff = new BufferedReader(input);
+        
+        int A, B, X;
+        A = Integer.parseInt(buff.readLine()); B = Integer.parseInt(buff.readLine());
+        X = A + B;
+        
+        System.out.println("X = " + X);
     }
 }
